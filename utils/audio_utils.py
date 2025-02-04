d57e3ef1902c18223962208b3561679d68be8967
DevAppsDPM <devappsdpm@gmail.com>
2025-02-04 18:05:24 +0100

    cookies de chrome test

--- utils/audio_utils.py
+++ utils/audio_utils.py
@@ -11,6 +11,7 @@ async def fetch_audio_data(url):
         }],
         'quiet': True,
         'default_search': 'auto',
+        'cookies-from-browser': 'chrome',  # Usa cookies desde Chrome automáticamente
     }
     loop = asyncio.get_event_loop()
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
